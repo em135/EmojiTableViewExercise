18e1991d84e3f51398f8dd6b73b8a1b317f9e753
Emil Nielsen <emilqz@gmail.com>
2019-10-31 10:24:28 +0100

    Initial Commit

---
+++ TableViewExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewExercise
+//
+//  Created by Emil Nielsen on 31/10/2019.
+//  Copyright © 2019 Emil Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
